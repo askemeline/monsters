163ca0acf1d2d160aeba63f8ec61d5d43cd65974
Emeline Garo <emeline.garo@supinternet.fr>
2019-10-04 19:28:38 +0200

    Updates

---
+++ precache-manifest.d72f08304126dfd00334292f74ae9bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2fe5f238e8462ae2d0976ebe204f83",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "96cf51c199aab99f10e8",
+    "url": "/monsters/static/css/main.3116f802.chunk.css"
+  },
+  {
+    "revision": "fe60e8fdfee2abf24334",
+    "url": "/monsters/static/js/2.c3ef4538.chunk.js"
+  },
+  {
+    "revision": "96cf51c199aab99f10e8",
+    "url": "/monsters/static/js/main.5ad2fdec.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
